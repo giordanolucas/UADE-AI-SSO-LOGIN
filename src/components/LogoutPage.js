ec97c7963602c28abb84534767175a21711e5a99
Lucas Giordano <lucas.giordano@mulesoft.com>
2019-08-23 20:37:19 -0300

    Add logout page

--- src/components/LogoutPage.js
+++ src/components/LogoutPage.js
@@ -1,8 +1,28 @@
 import React from "react";
+import lscache from "lscache";
+import {parseQuery} from "../utils/Functions";
 
 class LogoutPage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = this.initializeFromQuery();
+  }
+
+  initializeFromQuery = () => {
+    let query = parseQuery(this.props.location.search);
+    let tenant = query.tenant;
+    let redirect = query.redirect;
+
+    if(!tenant || !redirect){
+      window.location.replace("error?error=La URL de login debe incluír tenant y redirect");
+    }
+
+    return { tenantId: tenant, redirectUri: redirect};
+  };
+
+  componentDidMount() {
+    lscache.remove(this.state.tenantId);
+    window.location.replace(this.state.redirectUri);
   }
 
   render() {
