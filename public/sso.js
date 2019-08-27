150e78dbf96e019f17e0f3393ffe90cbe96d8ac4
Lucas Giordano <lucas.giordano@mulesoft.com>
2019-08-26 21:22:22 -0300

    Add SSO js lib

---
+++ public/sso.js
@@ -0,0 +1,45 @@
+import lscache from "lscache";
+import jwtDecode from "jwt-decode";
+
+const BASE_SSO_URL = "https://uade-sso-login.herokuapp.com";
+const LOCALSTORAGE_USER_KEY = "user";
+
+function _getLoginUrl(tenantId, redirectUri){
+    return BASE_SSO_URL + "/login?tenant=" + tenantId + "&redirect=" + redirectUri;
+}
+
+function _getLogoutUrl(tenantId, redirectUri){
+    return BASE_SSO_URL + "/logout?tenant=" + tenantId + "&redirect=" + redirectUri;
+}
+
+function _saveUserToken(encodedToken){
+    let jsonToken = jwtDecode(encodedToken);
+
+    let expireDate = 0;
+    if(jsonToken["exp"]){
+        expireDate = new Date(jsonToken["exp"] * 1000);
+    }
+
+    let expireTime = 0;
+    if(expireDate > 0){
+        expireTime = expireDate - new Date();
+    }
+
+    lscache.set(LOCALSTORAGE_USER_KEY, jsonToken, expireTime);
+}
+
+export function login(tenantId, callbackUrl){
+    window.location.replace(_getLoginUrl(tenantId, callbackUrl));
+}
+
+export function saveUserToken(){
+    _saveUserToken(window.location.hash.substring(1));
+}
+
+export function getUserToken(){
+    return lscache.get(LOCALSTORAGE_USER_KEY);
+}
+
+export function logOut(tenantId, callbackUrl){
+    window.location.replace(_getLogoutUrl(tenantId, callbackUrl));
+}
