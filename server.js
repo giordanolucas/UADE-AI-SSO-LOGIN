750850ffa1169c873b94bfda356fbdcc27dbb605
Facundo Poblete <facundo1792@gmail.com>
2019-08-26 23:28:56 -0300

    Express add /public/sso.js route

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app = express();
 // the __dirname is the current directory from where the script is running
 app.use(express.static(path.resolve(__dirname, "build")));
 
+
+app.get("/public/sso.js", (req, res) => {
+  res.setHeader('Content-type', "text/javascript; charset=UTF-8");
+  res.sendFile(path.resolve(path.resolve(__dirname, "public"), "sso.js"));
+});
+
 // send the user to index html page inspite of the url
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(path.resolve(__dirname, "build"), "index.html"));
